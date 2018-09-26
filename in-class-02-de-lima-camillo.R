9e5556bebf2fa8d6c68466e0fdf35676f6e092b2
Lucas Camillo <lucas_camillo@brown.edu>
2018-09-26 13:33:54 -0400

    Complete

--- in-class-02-de-lima-camillo.R
+++ in-class-02-de-lima-camillo.R
@@ -36,10 +36,9 @@ hist(rain.df.fixed$daily, main = "Histogram of rain in Canada (1960-1980)", xlab
 
 
 #Part 2
-
+install.packages("nycflights13")
 library(nycflights13)
 library(dplyr)
-flights
 
 #1)
 flights %>%
@@ -58,8 +57,65 @@ flights %>%
   arrange(desc(arr_delay))
 
 #5)
+
+#6)
 flights %>%
-  
+  select(tailnum)
+
+#7)
+colnames(flights)[12] = "tail_num"
+
+#8)
+flights %>%
+  mutate(gain = arr_delay - dep_delay)
+
+#9)
+flights %>%
+  mutate(speed = distance/air_time*60)
+
+#10)
+flights %>%
+  group_by(flight) %>%
+  summarise(avg = mean(arr_delay, na.rm = TRUE))
+
+#11)
+flights %>%
+  group_by(flight) %>%
+  summarise(count = n(), dist = mean(distance, na.rm = TRUE), delay = mean(arr_delay, na.rm = TRUE)) %>%
+  ggplot(aes(dist,delay)) +
+  geom_point(aes(size = count), alpha = 1/2) +
+  geom_smooth() +
+  scale_size_area()
+
+#12)
+flights %>%
+  group_by(dest) %>%
+  summarise(n_flights = n_distinct(flight), n_planes = n_distinct(tail_num))
+
+#13)
+#a- this function is not very legible and is difficult to understand what is going on
+#b- very difficult
+#c- 
+flights %>%
+  group_by(year, month, day) %>%
+  select(arr_delay, dep_delay) %>%
+  summarise(arr = mean(arr_delay, na.rm = TRUE), dep = mean(dep_delay, na.rm = TRUE)) %>%
+  filter(arr > 30 | dep > 30)
+#d- it giver the same results
+
+#14)
+#a- hard to read and understand what it is supposed to do
+#b- very difficult
+#c- 
+hourly_delay = flights %>%
+  filter(!is.na(dep_delay)) %>%
+  group_by(year, month, day, hour) %>%
+  summarise(delay = mean(dep_delay), n=n()) %>%
+  filter(n>10)
+#d- it gives the same results
+
+
+
 
 
 
